8167a625aadfe2640ef4f674956df5ba50b42165
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-18 04:05:04 +0100

    Add cors option

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ const PORT = process.env.PORT || 5500;
 const app = express();
 const cache = apicache.middleware;
 
+const corsOption = {
+  origin: 'https://zekumoru.github.io/',
+  optionsSuccessStatus: 200,
+};
+
 app.use(cache('2 hours'));
-app.use(cors());
+app.use(cors(corsOption));
 app.use('/game-store-api', router);
 
 app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
